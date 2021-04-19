61e2515a1f979a85961016439e1c46e28db665fb
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-19 10:31:15 -0300

    Adds GET and POST routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
-import express from "express";
+import express from "express"
 
 const app = express()
+
+/** Métodos HTTP
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deltar
+ * PATCH = Alterar uma informação específica
+*/
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Olá NLW 05"
+  })
+})
+
+app.post("/users", (request, response) => {
+  return response.json({
+    message: "Usuário salvo com sucesso!"
+  })
+})
 
 app.listen(3333, () => console.log("[server] running on http://localhost:3333"))
