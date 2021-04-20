112a4997f2d4587b24b2a5c0cc31c4da25677924
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-20 11:55:25 -0300

    Adds settings controller

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,11 @@
 import express from "express"
 
 import "./database"
+import { routes } from "./routes"
 
 const app = express()
 
-/** Métodos HTTP
- * GET = Buscas
- * POST = Criação
- * PUT = Alteração
- * DELETE = Deltar
- * PATCH = Alterar uma informação específica
-*/
-
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW 05"
-  })
-})
-
-app.post("/users", (request, response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso!"
-  })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => console.log("[server] running on http://localhost:3333"))
