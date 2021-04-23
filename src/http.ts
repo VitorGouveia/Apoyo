e3955686af5d09f95cf0d4563f9270b31bf4f43d
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-23 10:55:31 -0300

    Adds Socket.io connection between node.js and client

---
+++ src/http.ts
@@ -0,0 +1,32 @@
+import express from "express"
+import path from "path"
+
+import { createServer } from "http"
+import { Server, Socket } from "socket.io"
+
+import "./database"
+import { routes } from "./routes"
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.set("views", path.join(__dirname, "..", "public"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engine", "html")
+
+app.get("/", (request, response) => {
+  return response.render("html/client.html")
+})
+
+const http = createServer(app) // Criando protocolo http
+const io = new Server(http) // Criando protocolo websocket
+
+// io.on("connection", (socket: Socket) => {
+//   console.log("Se conectou", socket.id)
+// })
+
+app.use(express.json())
+
+app.use(routes)
+
+export { http, io }
