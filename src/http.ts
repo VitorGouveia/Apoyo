f23cc26ae288b636917d7753fe6acc7832d0f969
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-25 12:36:35 -0300

    Adds cors + bugfix chat button

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import path from "path"
+import cors from "cors"
 
 import { createServer } from "http"
 import { Server, Socket } from "socket.io"
@@ -13,6 +14,7 @@ app.use(express.static(path.join(__dirname, "..", "public")))
 app.set("views", path.join(__dirname, "..", "public"))
 app.engine("html", require("ejs").renderFile)
 app.set("view engine", "html")
+app.use(cors())
 
 app.get("/", (request, response) => {
   return response.render("html/client.html")
